c53598e96e5de2db3960d8301e9bbca708437716
Gerson <ggarridom@uni.pe>
2017-05-25 02:28:22 -0500

    adding things

--- helpers/developers.js
+++ helpers/developers.js
@@ -4,7 +4,13 @@ module.exports = {
 
         var developers = this.getAllDevelopers();
 
-        var developer = developers[id];
+        var developersWithId = developers.filter(function(developer){
+
+            return(developer.id === id);
+
+        });
+
+        var developer = developersWithId[0];
 
         if(developer === undefined){
             return{
@@ -18,24 +24,32 @@ module.exports = {
     },
     getAllDevelopers: function (){
 
-        return {
-
-            'ryan': {
+        return [
+            {
+                id: 'ryan',
                 name: 'Ryan Haskell-Glatz',
                 favoriteLanguage: 'Elm',
                 image: 'https://onenorth.blob.core.windows.net/keystone/ryan2.png'
             },
-            'erik': {
+            {
+                id: 'erik',
                 name: 'Erik Carron',
                 favoriteLanguage: 'Elm',
                 image: 'https://onenorth.blob.core.windows.net/keystone/Erik-Carron_BW_Cropped.jpg'
             },
-            'emma': {
+            {
+                id: 'emma',
                 name: 'Emma Birdsong',
                 favoriteLanguage: 'C#',
                 image: 'https://onenorth.blob.core.windows.net/keystone/emma2.png'
+            },
+            {
+                id: 'alex-hawley',
+                name: 'Alex Hawley',
+                favoriteLanguage: 'Elm',
+                image: 'https://onenorth.blob.core.windows.net/keystone/Alex_Hawley.jpg'
             }
-        };
+        ];
     }
 
 };
