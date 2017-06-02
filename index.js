8e23333b8bcbe17d183888b787b54dd4ffcf9fcf
Gerson <ggarridom@uni.pe>
2017-06-02 05:23:35 -0500

    addig socket-io

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require('express'),
     app = express(),
     pug = require('pug');
 
+var server = require('http').Server(app);
+var port = process.env.PORT || 3000;
+
+var io = require('socket.io')(server);
+
+server.listen(port, function () {
+    console.log("Listening on *:" + port);
+});
+
 var developersLanding = require('./developers-landing'),
     developersDetail = require('./developers-detail');
 
@@ -18,8 +27,33 @@ app.get('/developers', developersLanding.controller);
 
 app.get('/developers/:developerId', developersDetail.controller);
 
-app.listen(3000, function() {
+io.on('connection', function (socket) {
+    console.log('A user connected:' + socket.id);
 
-    console.log('Web app ready at port 3000!');
+    //Tell all clients that someone connected
+    io.emit('user joined', socket.id)
 
-})
+    // The client sends 'chat.message' event to server
+    socket.on('chat.message', function (message) {
+        //Emit this event to all clients connected to it
+        io.emit('chat.message', message);
+    });
+
+    //client sends "user typing" event to server
+    socket.on('user typing', function (username) {
+        io.emit('user typing', username);
+    });
+
+    //client sends 'stopped typing' event to server
+    socket.on('stopped typing', function (username) {
+        io.emit('stopped typing', username);
+    });
+
+
+    socket.on('disconnect', function () {
+        console.log('User left: ' + socket.id);
+
+        //Tell all clients that someone disconnected
+        socket.broadcast.emit('user left', socket.id);
+    });
+});
